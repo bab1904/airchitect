1e7fcadc3e0b387f5e2684bbba0916cd0309fc4e
balcha venkata yaswath kumar <yaswanthkumarb7@gmail.com>
2026-08-30 12:15:17 +0530

    refactor: clean codebase, enable noEmit and remove artifacts

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        include: ['components/**/__tests__/*.{test,spec}.{ts,tsx}'],
-        globals: true,
-        setupFiles: 'test/setup.ts'
-    }
-});
